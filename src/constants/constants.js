5da0215fb32293189f2ec259af6abe5f05d6261d
Jaime Trevino <trevinojaime42@gmail.com>
2021-09-16 23:30:12 -0400

    day 1 done

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,46 +1,18 @@
 export const projects = [
   {
-    title: 'MERN Memories',
-    description: "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
-      image: '/images/1.png',
-      tags: ['Mongo', 'Express', 'React', 'Node'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
+    title: 'DOD 4th Medical Group',
+    description: "The app includes features such as: dual language accessibility, night shift mode, geo-location, web portal access, and a sleek UI.",
+      image: '/images/dod4.JPG',
+      tags: ['React Native', 'Android Studio', 'Swift', 'Expo'],
+    source: 'https://docs.google.com/presentation/d/13aqSPiWbtGwWNXUu5Y761GB4w9bLUFdHS_PV5tdrxmM/edit#slide=id.g7187a9d989_2_166',
+    visit: 'https://github.com/choojoe/dod-4th-medical-team',
     id: 0,
   },
-  {
-    title: 'E-Commerce',
-    description:"While building it you're going to learn many advanced React & JavaScript topics, as well as how to use Stripe for card transactions. On top of that, at the end of the video, you will have this unique and complex webshop app that you will be able to add to your portfolio. And trust me, e-commerce applications are impressive.",
-    image: '/images/2.png',
-    tags: ['React', 'JavaScript'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 1,
-  },
-  {
-    title: 'WebRTC App',
-    description: "This is a code repository for the corresponding YouTube video. In this tutorial, we're going to build and deploy a React Video Chat Application using WebRTC.",
-      image: '/images/3.jpg',
-      tags: ['React', 'WebRTC'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 2,
-  },
-  {
-    title: 'Unichat',
-    description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-    image: '/images/4.jpg',
-    tags: ['React', 'ChatEngine', 'Firebase'],
-    source: 'https://google.com',
-    visit: 'https://google.com',
-    id: 3,
-  },
+ 
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2019, text: 'Finished my major in Linguistics and began my journey in Computer Science',},
+  { year: 2020, text: 'Worked as a full stack engineer at my first internship', },
+  { year: 2021, text: 'Graduated from Duke and worked on extensive projects', },
 ];
